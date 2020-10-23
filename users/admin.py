7f6c39dc20657fb781dd375decd639c7eee03353
Dilain Gunasekara <adsgunasekara18@gmail.com>
2020-10-23 16:33:50 +0530

    Initial commit

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Profile
 
+admin.site.site_header = "Field Engineer Management Admin"
+admin.site.site_title = "Admin Area"
+admin.site.index_title = "Welcome to Field Engineer Management Admin Area"
+
 admin.site.register(Profile)
